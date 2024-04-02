2de6670e900465ad5b220d38ae0b66b903c9046f
deborah-andrade <deborahandraderibeiro44@gmail.com>
2024-04-02 15:40:41 -0300

    ref: initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+export const  App =() => { //MELHOR DE USAR POIS AJUDA MAIS NA IMPORTAÇÃO NO INDEX.TSX
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Olá, primeiras alterações feitas.
     </div>
   );
 }
 
-export default App;
